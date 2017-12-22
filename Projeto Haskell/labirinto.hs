e291e3367b00b4954a0cbb6826ab43e8ebb2561f
Hebert Morais <hebertlucashllm@gmail.com>
2017-12-22 12:34:59 -0300

    Adicionado imprimir pontos e passagem de fases

--- Projeto Haskell/labirinto.hs
+++ Projeto Haskell/labirinto.hs
@@ -46,32 +46,61 @@ main = do
     printLabel
     threadDelay  2000000 
     limpaTela
-    let maze = [0,1,1,1,1,1,1,1,0,0,0,0,2,0,0,1,1,0,0,2,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,3,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1 ]
-    let items = [13,20]
+    let maze1 = [0,1,1,1,1,1,1,1
+                ,0,0,0,1,2,1,2,1
+                ,1,0,1,2,1,0,0,1
+                ,1,0,0,0,1,0,0,1
+                ,1,0,1,1,1,0,0,1
+                ,1,0,0,0,1,2,0,3
+                ,1,0,1,0,0,0,0,1
+                ,1,1,1,1,1,1,1,1 ]
+    let items1 = [15,13,46,20]
+
+    let maze2 = [0,1,1,1,0,1,1,1
+                ,0,0,0,0,1,1,0,1
+                ,1,0,0,1,2,1,0,3
+                ,1,0,1,1,0,1,0,1
+                ,1,0,0,0,0,0,2,1
+                ,1,0,1,0,1,1,0,1
+                ,0,1,1,1,2,0,0,1
+                ,1,0,0,0,1,1,1,0 ]
+    let items2 = [21,53]
+
+    
+    let mazes = [(maze1,items1),(maze2,items2)]
     let start = 1
     let pos = 1
     let points = 0
-    game maze start pos points items
+    game mazes maze1 start pos points items1
 
-game :: [Int] -> Int -> Int -> Int -> [Int] -> IO()
-game maze start pos points items = do
-    show_maze maze start pos items  
-    putStrLn "Digite seu comando (a,w,s,d) e aperte enter"
-    c <- getLine
+game :: [([Int],[Int])] -> [Int] -> Int -> Int -> Int -> [Int] -> IO()
+game mazes maze start pos points items = do
 
-    putStr "\n--------\n"
-    putStr "Pontos :"
-    print points
-    limpaTela
-    
+    if start < 0 then do
+        putStr "\n\n---JOGO FINALIZADO----\n\n"
+        putStr "Pontos :"
+        print points
+        
+    else do
+        show_maze maze start pos items  
 
-    if (pos `elem` items) then do
-        let newPoints = points + (pos * 10)        
-        walk game maze start pos newPoints c (remove pos items)
-    else
-         walk game maze start pos points c items
+        putStr "\n--------\n"
+        putStr "Pontos :"
+        print points
+        putStr "--------\n"
+        putStrLn "Digite seu comando (a,w,s,d) e aperte enter"
+        c <- getLine
+        
+        limpaTela
+        
 
-walk game maze start pos points input items = do
+        if (pos `elem` items) then do
+            let newPoints = points + (pos * 10)        
+            walk game mazes maze start pos newPoints c (remove pos items)
+        else
+            walk game mazes maze start pos points c items
+
+walk game mazes maze start pos points input items = do
     let tmpd = drop pos maze
     let tmp1 = head tmpd
     let tmps = drop (pos + 7) maze
@@ -81,21 +110,107 @@ walk game maze start pos points input items = do
     let tmpa = drop (pos - 2) maze
     let tmp4 = head tmpa
     
-    let walkableRight = tmp1 /= 1
-    let walkableDown = tmp2 /= 1
-    let walkableUp= tmp3 /= 1
-    let walkableLeft = tmp4 /= 1
+    let walkableRight = tmp1 /= 1 && mod pos 8 /= 0
+    let walkableDown = tmp2 /= 1 && ((pos + 8) < 64)
+    let walkableUp= tmp3 /= 1 && pos > 8
+    let walkableLeft = tmp4 /= 1 && pos > 0
 
     if input == "s" && walkableDown then do
-        game maze start (pos + 8) points items
-    else if input == "w" && walkableUp then 
-        game maze start (pos - 8) points items
-    else if input == "d" && walkableRight then 
-        game maze start (pos + 1) points items
-    else if input == "a" && walkableLeft then 
-        game maze start (pos - 1) points items
+        let newPos = pos + 8
+        let newMaze = maze
+        let newMazes = mazes
+        let newStart = start
+        let newItems = items
+        if ( maze !! newPos - 1 == 3) then do
+            let newMazes = tail mazes 
+            let newMaze = fst (head newMazes)
+            let newPos = 1
+            let newStart = 1
+            let newItems = snd (head newMazes)
+            putStr "\n\n--PASSOU DE FASE--\n\n"
+            limpaTela
+            threadDelay  1200000     
+            if (length newMazes == 0) then do
+                let newStart = -1
+                game newMazes newMaze newStart newPos points newItems  
+            else
+                 game newMazes newMaze newStart newPos points newItems
+            
+        else
+            game newMazes newMaze newStart newPos points newItems
+    else if input == "w" && walkableUp then do
+        let newPos = pos - 8
+        let newMaze = maze
+        let newMazes = mazes
+        let newStart = start
+        let newItems = items
+        if ( maze !! newPos - 1 == 3) then do
+            let newMazes = tail mazes 
+            let newMaze = fst (head newMazes)
+            let newPos = 1
+            let newStart = 1
+            let newItems = snd (head newMazes)
+            putStr "\n\n--PASSOU DE FASE--\n\n"
+            limpaTela
+            threadDelay  1200000     
+            if (length newMazes == 0) then do
+                let newStart = -1
+                game newMazes newMaze newStart newPos points newItems  
+            else
+                 game newMazes newMaze newStart newPos points newItems        
+            
+        else
+            game newMazes newMaze newStart newPos points newItems
+
+    else if input == "d" && walkableRight then do
+        let newPos = pos + 1
+        let newMaze = maze
+        let newMazes = mazes
+        let newStart = start
+        let newItems = items
+        if ( maze !! (newPos - 1) == 3) then do
+            let newMazes = tail mazes 
+            let newMaze = fst (head newMazes)
+            let newPos = 1
+            let newStart = 1
+            let newItems = snd (head newMazes)
+            putStr "\n\n--PASSOU DE FASE--\n\n"
+            limpaTela
+            threadDelay  1200000     
+            if (length newMazes == 0) then do
+                let newStart = -1
+                game newMazes newMaze newStart newPos points newItems  
+            else
+                 game newMazes newMaze newStart newPos points newItems     
+            
+        else
+            game newMazes newMaze newStart newPos points newItems
+    else if input == "a" && walkableLeft then do
+        let newPos = pos - 1
+        let newMaze = maze
+        let newMazes = mazes
+        let newStart = start
+        let newItems = items
+        if ( maze !! newPos == 3) then do
+            let newMazes = tail mazes 
+            let newMaze = fst (head newMazes)
+            let newPos = 1
+            let newStart = 1
+            let newItems = snd (head newMazes)
+            putStr "\n\n--PASSOU DE FASE--\n\n"
+            limpaTela
+            threadDelay  1200000     
+            if (length newMazes == 0) then do
+                let newStart = -1
+                game newMazes newMaze newStart newPos points newItems  
+            else
+                 game newMazes newMaze newStart newPos points newItems
+                    
+            
+        else
+            game newMazes newMaze newStart newPos points newItems
     else
-        game maze start pos points items  
+        game mazes maze start pos points items  
 
 show_maze :: [Int] -> Int -> Int -> [Int] -> IO()
 show_maze maze counter pos items = do
