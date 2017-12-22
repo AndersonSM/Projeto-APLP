1ef6e456373b232732b430d989648d966e2977ec
Hebert Morais <hebertlucashllm@gmail.com>
2017-12-22 00:51:21 -0300

    Adicionado pontos e recolhendo os items
    Foi subido em cima do commit de helder pois nao to conseguindo baixar as dependencias para o modulo de console, ja foi comunicado ao mesmo que isso aconteceria e ele tomara medidas cabiveis
    
    pas

--- Projeto Haskell/labirinto.hs
+++ Projeto Haskell/labirinto.hs
@@ -1,23 +1,32 @@
 import System.IO
-import System.Console.ANSI
 
-limpaTela :: IO ()
-limpaTela = do
-    clearScreen
+main :: IO ()
+main = do
+    let maze = [0,1,1,1,1,1,1,1,0,0,0,0,2,0,0,1,1,0,0,2,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,3,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1 ]
+    let items = [13,20]
+    let start = 1
+    let pos = 1
+    let points = 0
+    game maze start pos points items
 
-game :: [Int] -> Int -> Int -> IO()
-game maze start pos = do
-    show_maze maze start pos    
+game :: [Int] -> Int -> Int -> Int -> [Int] -> IO()
+game maze start pos points items = do
+    show_maze maze start pos items  
     putStrLn "Digite seu comando (a,w,s,d) e aperte enter"
     c <- getLine
-    putStr "\n--------\n"
-    limpaTela
-    let newpos = pos + 1
-    let tmp = drop pos maze
-    let tmp1 = head tmp
-    walk game maze start pos c
 
-walk game maze start pos input = do
+    putStr "\n--------\n"
+    putStr "Pontos :"
+    print points
+    
+
+    if (pos `elem` items) then do
+        let newPoints = points + (pos * 10)        
+        walk game maze start pos newPoints c (remove pos items)
+    else
+         walk game maze start pos points c items
+
+walk game maze start pos points input items = do
     let tmpd = drop pos maze
     let tmp1 = head tmpd
     let tmps = drop (pos + 7) maze
@@ -33,55 +42,44 @@ walk game maze start pos input = do
     let walkableLeft = tmp4 /= 1
 
     if input == "s" && walkableDown then do
-        game maze start (pos + 8)
+        game maze start (pos + 8) points items
     else if input == "w" && walkableUp then 
-        game maze start (pos - 8)
+        game maze start (pos - 8) points items
     else if input == "d" && walkableRight then 
-        game maze start (pos + 1)
+        game maze start (pos + 1) points items
     else if input == "a" && walkableLeft then 
-        game maze start (pos - 1)
+        game maze start (pos - 1) points items
     else
-        game maze start pos
+        game maze start pos points items  
 
-drawPlayer :: IO ()
-drawPlayer = do
-    setSGR [SetConsoleIntensity BoldIntensity]
-    setSGR [SetColor Foreground Vivid Blue]
-    putStr "@"
-    setSGR [Reset]
-
-drawBonus :: IO ()
-drawBonus = do
-    setSGR [SetConsoleIntensity BoldIntensity]
-    setSGR [SetColor Foreground Vivid Red]   
-    putStr "*"
-    setSGR [Reset] 
-
-show_maze :: [Int] -> Int -> Int -> IO()
-show_maze maze counter pos = do
+show_maze :: [Int] -> Int -> Int -> [Int] -> IO()
+show_maze maze counter pos items = do
     let h = head maze
-    decide h pos counter
+    decide h pos counter items
     putStr " "
     let counter1 = counter + 1
     let maze1 = tail maze
     cr_if_mult_8 counter
     if not ( counter == 64 ) then
-        show_maze maze1 counter1 pos
+        show_maze maze1 counter1 pos items
     else
         putStr ""
 
-decide :: Int -> Int -> Int -> IO()
-decide h pos counter = do
+decide :: Int -> Int -> Int -> [Int] -> IO()
+decide h pos counter items = do
     if counter == pos then
-        drawPlayer
-    else if h == 0 then
-        putStr "."
-    else if h == 2 then
-        drawBonus
+        putStr "@"
+    else if h == 1 then
+        putStr "#"
+    else if h == 2 && (counter `elem` items) then
+        putStr "*"
     else if h == 3 then
         putStr "$"
     else
-        putStr "#"
+        putStr "."
+
+
+remove element list = filter (\e -> e/=element) list        
 
 cr_if_mult_8 :: Int -> IO()
 cr_if_mult_8 c = do
@@ -90,37 +88,3 @@ cr_if_mult_8 c = do
         putStrLn ""
     else
         putStr ""
-
-main :: IO ()
-main = do
-    setTitle "O Labirinto"
-    let maze = [0,1,1,1,1,1,1,1,
-                0,0,0,0,0,0,0,1,
-                1,0,1,1,0,1,0,1,
-                1,0,1,0,0,1,2,1,
-                1,0,1,2,1,1,1,1,
-                1,0,1,1,1,0,0,3,
-                1,0,0,0,0,0,1,1,
-                1,1,1,1,1,1,1,1]
-    
-    let maze2 = [0,1,1,1,1,1,1,1,
-                0,0,0,0,0,0,0,1,
-                1,0,0,0,2,0,0,1,
-                1,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,3,
-                1,0,0,0,0,0,0,1,
-                1,1,1,1,1,1,1,1]
-    
-    let maze3 = [0,1,1,1,1,1,1,1,
-                0,0,0,0,0,0,0,1,
-                1,0,0,0,2,0,0,1,
-                1,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,1,
-                1,0,0,0,0,0,0,3,
-                1,0,0,0,0,0,0,1,
-                1,1,1,1,1,1,1,1]
-
-    let start = 1
-    let pos = 1
-    game maze start pos
